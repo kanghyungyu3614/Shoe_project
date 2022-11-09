e864e3751d52a25b2fe2ff8da7fe0f2a18f0da5e
kimkyoungju <rlarudwn900@naver.com>
2022-11-09 16:14:06 +0900

    111

--- shoesproject/src/main/java/model/dao/DetailDao.java
+++ shoesproject/src/main/java/model/dao/DetailDao.java
@@ -30,4 +30,35 @@ public class DetailDao extends dao {
 		return null;
 	}
 	
+	
+	public boolean selin(int hideNum , int selprice, String lid) {
+		//String spstatus ="판매입찰";
+		String sql = "insert into sproduct(spstatus,spsize,spsellid,spprice) values ('판매입찰',?,?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, hideNum);
+			ps.setInt(3, selprice);
+			ps.setString(2, lid);
+			ps.executeUpdate();
+			
+			return true;	
+		} catch (Exception e) {
+			System.out.println(e);
+		}return false;
+	}
+	
+	public boolean selbuy(int hideNum , int selprice, String lid) {
+		String sql = "insert into sproduct(spstatus,spsize,spbuyid,spprice) values ('구매입찰',?,?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, hideNum);
+			ps.setInt(3, selprice);
+			ps.setString(2, lid);
+			ps.executeUpdate();
+			
+			return true;	
+		} catch (Exception e) {
+			System.out.println(e);
+		}return false;
+	}
 }
--- shoesproject/src/main/webapp/js/sellDetail.js
+++ shoesproject/src/main/webapp/js/sellDetail.js
@@ -1,10 +1,12 @@
 let callComponent = 0;
-let hideComponent;
+let hideComponent = 0;
 let hideNum = 0;
 function sellAddButton(num){
 	console.log(num);
 	hideComponent = document.querySelector("#size")
 	hideComponent.innerHTML = num;
+	
+	
 	let section = document.querySelector(".sellborder");
 	let mainsection = document.querySelector("#main");
 	let body = document.querySelector("body");
@@ -16,9 +18,9 @@ function sellAddButton(num){
 	callComponent += 1;
 	if(callComponent<2){
 		section.innerHTML += html;
-		mainsection.style="height: 903px"
-		section.style="height: 816px"
-		body.style = "height: 750px"
+		mainsection.style="height: 100px"
+		section.style="height: 100px"
+		body.style = "height: 100px"
 	}
 	
 }
@@ -27,3 +29,50 @@ function productSellButton(){
 	let hideNum = Number(hideComponent.innerHTML);
 	console.log(hideNum);
 }
+
+function selin(){
+	let selprice = document.querySelector(".selprice").value
+ 	  hideComponent = document.querySelector("#size");
+   	let hideNum = Number(hideComponent.innerHTML);
+	
+  	 console.log(hideNum+selprice);   
+  	 $.ajax({
+		url : "/shoesproject/product/detail",
+		type : "post",
+		data :  {"selprice" : selprice , "hideNum" : hideNum ,"type" : 1},
+		success : function(re){
+			if(re=="true"){
+				alert("등록성공")
+			}else{
+				alert("실패")
+			}
+			
+		}
+		
+	
+})
+   
+}
+function selbuy(){
+	let selprice = document.querySelector(".selprice1").value
+ 	  hideComponent = document.querySelector("#size");
+   	let hideNum = Number(hideComponent.innerHTML);
+	
+  	 console.log(hideNum+selprice);   
+  	 $.ajax({
+		url : "/shoesproject/product/detail",
+		type : "post",
+		data :  {"selprice" : selprice , "hideNum" : hideNum ,"type" : 2},
+		success : function(re){
+			if(re=="true"){
+				alert("등록성공")
+			}else{
+				alert("실패")
+			}
+			
+		}
+		
+})
+}
+	
+	
